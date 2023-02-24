7dbcd8644a006b19049729c93e2b3700e02e9e69
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-02-24 13:40:33 +0100

    if condition, ternary operator

--- day-4-start-example-2/src/App.js
+++ day-4-start-example-2/src/App.js
@@ -1,25 +1,54 @@
+// example 1
+// import './App.css';
+
+// function App() {
+//   let isLogged = false;
+
+//   let content;
+
+//   if (isLogged) {
+//     content = <div>
+//       <p>Name:</p>
+//       <p>Address:</p>
+//       <p>Phone:</p>
+//     </div>
+//   } else {
+//     content = <h1>Log in form</h1>
+//   }
+
+//   return (
+//     <div className="App">
+//       {content}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// example 2
 import './App.css';
 
 function App() {
-  let isLogged = false;
+  let isLogged = true;
 
   let content;
 
-  if (isLogged) {
-    content = <div>
-      <p>Name:</p>
-      <p>Address:</p>
-      <p>Phone:</p>
-    </div>
-  } else {
-    content = <h1>Log in form</h1>
-  }
-
+  // ternary operator
   return (
     <div className="App">
       {content}
+      <div>{isLogged ? <div>You are logged in</div> : <h1>Log in form</h1>}</div>
     </div>
-  );
+  )
 }
 
 export default App;
+
+// ternary oparator starting point
+/* <div className="App">
+  {content}
+  <div>{isLogged ? : }</div>
+</div>
+  ); */
