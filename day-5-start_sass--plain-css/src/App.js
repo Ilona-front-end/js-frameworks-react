c7d03a3cbd01a400011722dd0e95c213e93b3c72
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-02-28 12:40:15 +0100

    branch day 5 - folder day-5-start... sass usage with React (beginning)

---
+++ day-5-start_sass--plain-css/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.scss';
+import './sass/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>SASS</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
